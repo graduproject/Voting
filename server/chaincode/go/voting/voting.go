afefda9b1bb23c86a5ce7d778a62c92b4c9492bc
SangHyun Lee <jdsd2233@naver.com>
2018-09-03 16:13:24 +0900

    [TBC18-9] ...

--- server/chaincode/go/voting/voting.go
+++ server/chaincode/go/voting/voting.go
@@ -16,7 +16,7 @@ type Voting struct {
 	StartTime       int64       `json="starttime"`
 	EndTime         int64       `json="endtime"`
 	CurrentState    int         `json="currentstate"`  // state 0 : 투표 시작 전,   1 : 투표 가능,   2 : 투표 종료
-	Winner          string      `josn="winner"`        // 가장 표를 많이 받은 후보
+	Winner          string      `json="winner"`        // 표를 가장 많이 받은 후보
 }
 
 // votingSlice is ...
@@ -40,23 +40,24 @@ func votingInit(name string, startTime int64, endTime int64) {
 	votingSlice[num].StartTime = startTime
 	votingSlice[num].EndTime = endTime
 	votingSlice[num].CurrentState = 0
+	votingSlice[num].Winner = "null"
 }
 
 // registerCandidate register candidate in Voting structure
-func (v *Voting) registerCandidate(cd string) { // 후보 등록
+func (v *Voting) registerCandidate(cd string) { // 후보 등록, cd는 후보 이름
 	v.Candidate = append(votingSlice[v.VotingNumber-1].Candidate, cd)
 	v.Poll = append(votingSlice[v.VotingNumber-1].Poll, 0)
 }
 
-// GetCandidate gets candidate in Voting structure
+// getCandidate gets candidate in Voting structure
 func (v *Voting) getCandidate() { // 후보자 확인
 	for i := 0; i < len(v.Candidate); i++{
 		fmt.Println(v.Candidate[i])
 	}
 }
 
-// DeleteCandidate deletes candidate in Voting structure
-func (v *Voting) deleteCandidate(num int) {
+// deleteCandidate deletes candidate in Voting structure
+func (v *Voting) deleteCandidate(num int) { // num은 후보 번호
 	/*if v.StartTime < time.Now().Unix() { // 시작전에만 삭제 가능
 		fmt.Println("후보를 삭제할 수 없습니다")
 		return
@@ -71,7 +72,7 @@ func (v *Voting) deleteCandidate(num int) {
 	v.Poll = v.Poll[:len(v.Poll)-1]
 }
 
-// ViewPoll views Poll in Voting structure
+// viewPoll views Poll in Voting structure
 func (v *Voting) viewPoll() { // 득표 확인
 	for i := 0; i < len(v.Poll); i++{
 		fmt.Print(v.Poll[i], " ")
@@ -79,13 +80,13 @@ func (v *Voting) viewPoll() { // 득표 확인
 	fmt.Println()
 }
 
-// Vote increases Poll belong to selected candidate
-func (v *Voting) vote(num int) { // 투표
+// vote increases Poll belong to selected candidate
+func (v *Voting) vote(num int) { // 투표, num은 후보 번호
 	num--
 	v.Poll[num]++
 }
 
-// ChangeState change Voting structure's CurrentState
+// changeState change Voting structure's CurrentState
 func (v *Voting) changeState() { // Voting 상태 변화
 	// TODO : 어떻게 할지 생각해봐야함
 	if v.StartTime < time.Now().Unix() && v.EndTime > time.Now().Unix() { // 투표 시작
@@ -98,7 +99,7 @@ func (v *Voting) changeState() { // Voting 상태 변화
 	}
 }
 
-// ViewCompleteVoting views completed Voting
+// viewCompleteVoting views completed Voting
 func viewCompleteVoting() { // 전체 투표 목록 중 완료된 투표 조회
 	for i := 0; i < len(votingSlice); i++ {
 		if votingSlice[i].CurrentState == 2 { // 상태가 2인 투표들은 투표가 완료된 것들
@@ -107,21 +108,36 @@ func viewCompleteVoting() { // 전체 투표 목록 중 완료된 투표 조회
 	}
 }
 
-// SaveCompleteID saves ID
-func (v *Voting) SaveCompleteID() {
+// saveCompleteID saves ID
+func (v *Voting) saveCompleteID() {
 	// TODO : 투표 완료한 아이디 추가
 }
 
+// selectWinner selects winner
 func (v *Voting) selectWinner() {
+	var draw []string
 	var temp int
 	var tempIndex int
-	for i := 0; i < len(v.Poll); i++ {
+	for i := 0; i < len(v.Poll); i++ { // 최대 표와 최대 표를 받은 후보를 임시로 저장
 		if temp < v.Poll[i] {
 			temp = v.Poll[i]
 			tempIndex = i
 		}
 	}
-	v.Winner = v.Candidate[tempIndex]
+	for i := 0; i < len(v.Poll); i++ { // 최대 표와 동률인 표 검색
+		if temp == v.Poll[i] {
+			draw = append(draw, v.Candidate[i])
+		}
+	}
+
+	if len(draw) == 1 { // 동률이 없을 시 해당 후보가 당선
+		v.Winner = v.Candidate[tempIndex]
+	} else { // 동률이 있을 시 동률인 후보들 출력
+		for i := 0; i < len(draw); i++ { 
+			fmt.Print(draw[i], " ")
+		}
+		fmt.Println("draw")
+	}
 }
 
 func main() { // Test
@@ -129,6 +145,8 @@ func main() { // Test
 		votingSlice[0].registerCandidate("이상현")
 		votingSlice[0].registerCandidate("김도정")
 		votingSlice[0].registerCandidate("김현우")
+		votingSlice[0].registerCandidate("유상욱")
+		votingSlice[0].registerCandidate("최현빈")
 		fmt.Println(len(votingSlice[0].Candidate))
 		votingSlice[0].getCandidate()
 		votingSlice[0].deleteCandidate(2)
@@ -137,6 +155,10 @@ func main() { // Test
 		votingSlice[0].vote(1)
 		votingSlice[0].vote(1)
 		votingSlice[0].vote(1)
+		votingSlice[0].vote(1)
+		votingSlice[0].vote(4)
+		votingSlice[0].vote(4)
+		votingSlice[0].vote(4)
 		votingSlice[0].viewPoll()
 		votingSlice[0].selectWinner()
 		fmt.Println("Winner : ", votingSlice[0].Winner)
