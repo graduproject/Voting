a9962993005dfc6726729b10fd944eaa6f09ae58
DarrenKimBat <uiucpass@gmail>
2018-09-05 15:59:59 +0900

    Applying CSS

--- public/js/index.js
+++ public/js/index.js
@@ -5,21 +5,29 @@ var app = express();
 var path = require("path")
 var fs = require('fs');
 
-app.get('/', function(req, res){
-    fs.readFile(path.join(__dirname + '/../index.html'), function(error, data){
-        res.writeHead(200, {'Content-Type' : 'text/html; charset = utf-8'});
+app.use(express.static(path.join(__dirname + '/../../public')));
+
+app.get('/', function (req, res) {
+    fs.readFile(path.join(__dirname + '/../index.html'), function (error, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset = utf-8' });
         res.end(data);
     });
 });
 
-app.get('/images', function(req, res){
-    fs.readFile(path.join(__dirname + '/../images/main.png'), function(error, data){
-        res.writeHead(200, {'Content-Type' : 'text/html; charset = utf-8'});
+app.get('/vote', function (req, res) {
+    fs.readFile(path.join(__dirname + '/../index.html'), function (error, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset = utf-8' });
         res.end(data);
     });
 });
 
+app.get('/images', function (req, res) {
+    fs.readFile(path.join(__dirname + '/../images/main.png'), function (error, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset = utf-8' });
+        res.end(data);
+    });
+});
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Go!");
-  });
+});
