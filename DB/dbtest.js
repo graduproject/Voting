fa492be852cb6ad76da8de23a4384f1dd7e311b4
DarrenKimBat <uiucpass@gmail>
2018-09-08 13:44:01 +0900

    Temporary Database maked

---
+++ DB/dbtest.js
@@ -0,0 +1,16 @@
+var Client = require('mongodb').MongoClient;
+
+Client.connect('mongodb://localhost:27017/school', function(error, db){
+    if(error) {
+        console.log(error);
+    } else {
+        // 1. 입력할 documents 를 미리 생성
+        var one = {ID:'One', password: '1234'};
+        var two = {ID:'Two', password : '1234'};
+        var three = {ID:'Three', password : '1234'};
+
+        // 2. insertMany( ) 함수에 배열 형태로 입력
+        db.collection('student').insertMany([one, two, three]);
+        db.close();
+    }
+});
