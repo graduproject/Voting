4144c54c2e717d03e0cc6a08f15ace8928103e68
DarrenKimBat <uiucpass@gmail>
2018-09-28 07:40:18 -0700

    Chaincode function to Nodejs function

---
+++ public/js/command_config/voting_chaincode.js
@@ -0,0 +1,109 @@
+var docking = "docker exec cli ";
+var invoke = "peer chaincode invoke -o orderer.example.com:7050 --tls true --cafile ";
+var pemPath = "/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem ";
+var etc = "-C mychannel -n mycc -c ";
+
+function parse(parameter){
+	var args = Array.from(arguments[0]);
+	var temp = "[";
+
+	for(var i = 0; i < args.length; i++){
+		temp += '\"' + args[i] + '\"';
+		if(i != args.length - 1){
+			temp += ',';
+		}
+	}
+	temp += "]";
+	temp = "\'{\"Args\":" + temp + "}\' ";
+	temp = docking + invoke + pemPath + etc + temp;
+
+	return temp;
+}
+
+exports.createVoting = function(vnum, vname, vst_time, ved_time){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
+
+exports.changeState = function(last_vnum){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
+
+exports.registerCandidate = function(vnum,candidNum){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
+
+exports.vote = function(vnum, candidName, userID){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
+
+exports.queryAllVote = function(last_vnum){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
+
+exports.queryCompleteVote = function(last_vnum){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
+
+exports.earlyComplete = function(vnum){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
+
+exports.deleteCandidate = function(vnum, candidName){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
+
+exports.queryNotCompleteVote = function(last_vnum){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
+
+exports.queryCandidateWithPoll = function(last_vnum){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
+
+exports.queryCandidate = function(vnum){
+	var args = Array.from(arguments);
+	var syscmd = parse(args);
+	syscmd.toString();	
+	
+	return syscmd;
+}
