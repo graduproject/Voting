a3451a842d61f9c44f95de880f4a0dc0e8e15293
DarrenKimBat <uiucpass@gmail>
2018-09-28 22:22:09 -0700

    Path redirect

--- public/votingapp/index.js
+++ public/votingapp/index.js
@@ -1,7 +1,7 @@
 var sys = require('sys');
 var exec = require('child_process').exec;
-var temp = require('./js/command_config/voting_chaincode.js');
-var parser = require('./js/utils/parser.js');
+var temp = require('./chaincode/voting_chaincode.js');
+var parser = require('./utils/parser.js');
 var child;
 var tem;
 //var str = temp.createVoting("createVoting","1","first-vote","09/19/2018 3:20:00 PM","09/19/2018 8:20:00 PM");
@@ -10,7 +10,8 @@ sys.print(str.toString() + "\n");
 
 child = exec(str, function(error, stdout, stderr){
 	tem = stderr;
-	parser.read_parse(tem);
+	var s = parser.read_parse(tem);
+	console.log(s);
 	if(error != null){
 	//	console.log('exec error: ' + error);
 	}
